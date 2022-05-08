ed36d7923b18d873e71d5102af60da18d9ccedb8
mirasrobert <mrmirasrobert@gmail.com>
2022-05-08 12:46:36 +0800

    Add Secure Asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,11 +11,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
 </head>
 <body class="bg-gray-50">
 <div id="app"></div>
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
